ee14b80312090337112481cef1a7a725986334c6
r.aramide <r.aramide@lagetronix.com>
2024-02-05 16:55:44 +0100

    working...

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,43 @@
+// import { Stack } from "expo-router";
+// import Colors from "../constants/Colors";
+
+// const RootLayout = () =>{
+//     return <Stack>
+//         <Stack.Screen name="index" />
+//         <Stack.Screen name="job-details/[id]" />
+//     </Stack>
+// }
+
+// export default RootLayout;
+
+// Tutor Github code:
 import { Stack } from "expo-router";
-import Colors from "../constants/Colors";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
 
-const RootLayout = () =>{
-    return <Stack>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="job-details/[id]" /> 
+// SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "home",
+};
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack initialRouteName="home">
+      <Stack.Screen name="home" />
     </Stack>
-}
+  );
+};
 
-export default RootLayout;
+export default Layout;
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,34 +1,51 @@
-import React, {useState} from 'react'
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
-import { useRouter } from 'expo-router'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './popularjobs.style';
-import {COLORS, SIZES} from "../../../constants";
-import Popularjob from '../../common/cards/popular/PopularJobCard';
-import PopularJobCard from '../../common/cards/popular/PopularJobCard';
-import { useFetch } from '../../../hook/useFetch';
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import Popularjob from "../../common/cards/popular/PopularJobCard";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { useFetch } from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  
-  const {isLoading, error, data} = useFetch('search', {query: "React developer", page: "1", num_pages: "1"})
+
+  // const {isLoading, error, data} = useFetch('search', {query: "React developer", page: "1", num_pages: "1"})
+  const { isLoading, error, data } = useFetch("jobs");
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>
-        Popular Jobs
-        </Text>
-          <TouchableOpacity>
-        <Text style={styles.headerBtn}>Show all</Text>
-          </TouchableOpacity>
-        </View>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
 
-        <View style={styles.cardsContainer}>{isLoading ? <ActivityIndicator size={'large'} colors={COLORS.primary}/> : error ? <Text>An error occured</Text>: <FlatList data={data}
-        renderItem={({item})=><PopularJobCard item={item} />} keyExtractor={item=> item?.job_id} contentContainerStyle={{columnGap: SIZES.medium}} horizontal
-        />}</View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size={"large"} colors={COLORS.primary} />
+        ) : error ? (
+          <Text>An error occured</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
